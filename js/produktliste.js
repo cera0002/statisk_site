c79ecbf466b90c6cfba8ef2ead6c6ba8335af86f
cera0002 <cera0002@stud.kea.dk>
2025-02-03 15:36:49 +0100

    fix product

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,9 +1,9 @@
+const myCategory = new URLSearchParams(window.location.search).get("category");
+console.log("productliste loader...", myCategory);
+
 let listContainer = document.querySelector(".product_list_container");
 
-const urlParams = new URLSearchParams(window.location.search);
-const category = urlParams.get("category");
-
-fetch(`https://kea-alt-del.dk/t7/api/products/`)
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${myCategory}`)
   .then((response) => response.json())
 
   .then((data) => showList(data));
@@ -15,11 +15,11 @@ function showList(products) {
       (product) =>
         ` <div class="product_card">
           <p class="nr">//</p>
-          <a href="produkt.html" class="billede"> <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"
+          <a href="produkt.html?id=${product.id}" class="billede"> <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"
         alt="${product.productdisplayname}"/>   
-          <a href="produkt.html" class="navn">/ ${product.productdisplayname}</a>
-          <a href="produkt.html" class="pris">${product.price},-</a>
-          <a href="produkt.html" class="add">ADD TO BAG</a>
+          <a href="produkt.html?id=${product.id}" class="navn">/ ${product.productdisplayname}</a>
+          <a href="produkt.html?id=${product.id}" class="pris">${product.price},-</a>
+          <a href="produkt.html?id=${product.id}" class="add">ADD TO BAG</a>
         </div>`
     )
     .join("");
